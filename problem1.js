d50a132c3f0a3bdc86830a8148fd767b026ef3d1
ccastrom <claudiocastrom7@gmail.com>
2024-08-24 20:01:48 -0400

    Add missing information of 3 euler problems

--- problem1.js
+++ problem1.js
@@ -1,3 +1,9 @@
+/**If we list all the natural numbers below  that are multiples of 3 or 5 , we get  3,5,6 and 9
+ *  The sum of these multiples is 23
+ * 
+ * Find the sum of all the multiples of 3 or 5 below 1000
+ */
+
 var result=0;
 
 for (let index = 0; index <1000; index++) {
